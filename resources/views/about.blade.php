4a013a5612188d75636f1ae6a42d6beedc434264
Jelena Stojsin <ukradenitrenutak@gmail.com>
2018-08-02 01:50:30 -0700

    Zadatak 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+    <head>
+        <title> About us </title>
+
+        <style>
+                html, body {
+                    background-color: orange;
+                    color: green;
+                    font-family: 'Tangerine', sans-serif;
+                    font-weight: 1000;
+                    height: 100vh;
+                    margin: 30;
+                }
+        </style>
+    </head>
+
+    <body> 
+        <div class="flex-center position-ref full-height" align="center">
+            About us 
+        </div>
+    </body>
+
+</html>
